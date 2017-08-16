eae67fe5e3c71c19c6e219e33260aeb84dec23bb
bluscreenofjeff <jeffdimmock@ip-172-21-0-10.ec2.internal>
2017-08-16 01:01:14 -0700

    Initial script commit

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
 # Malleable-C2-Randomizer
-A script to randomize Cobalt Strike Malleable C2 profiles and reduce the chances of flagging signature-based detection controls
+This script randomizes Cobalt Strike Malleable C2 profiles through the use of a metalanguage, hopefully reducing the chances of flagging signature-based detection controls. In short, the script parses the provided template, substitutes the variables for a random value from either a provided or built-in wordlist, tests the new template with *c2lint*, and (if there are no *c2lint* errors) outputs the new Malleable C2 file.
+
+Sample Malleable C2 profiles that are compatible with this script can be found in the [Sample Templates](LINK-HERE) directory of this repo.
+
+For more detailed information about the script's usage and how to implement the metalanguage in an existing Malleable C2 profile, check out my blog post [BLAHBLAHBLAH](LINK-HERE).
+
+# Table of Contents
+- [Script Syntax](#script-syntax)
+    - [Basic Options](#basic-options)
+    - [Custom Wordlists](#custom-wordlists)
+- [Substitution Metalanguage](#substitution-metalanguage)
+    - [List of Variables](#list-of-variables)
+    - [Building Wordlists](#building-wordlists)
+- [Example Template and Output](#example-template-and-output)
+- [Further Resources](#further-resources)
+
+# Script Syntax
+
+```python
+python malleable-c2-randomizer.py [-h] -profile PROFILE
+                                  [-count COUNT]
+                                  [-cobalt COBALT]
+                                  [-output OUTPUT]
+                                  [-notest]
+                                  [-charset CHARSET]
+                                  [-wordlist WORDLIST]
+                                  [-useragent USERAGENT]
+                                  [-spawnto SPAWNTO]
+                                  [-pipename PIPENAME]
+                                  [-pipename_stager PIPENAME_STAGER]
+                                  [-dns_stager_subhost DNS_STAGER_SUBHOST]
+```
+
+## Basic Options
+| Parameter | Description |
+| ----- | ----- |
+| -profile, -p | Path to the Malleable C2 template to randomize (REQUIRED) |
+| -count, -c | The number of randomized profiles to create {Default = 1} |
+| -cobalt, -d | The directory where Cobalt Strike is located (for c2lint) {Default = current directory} |
+| -output, -o | Output base name {Default = template basename and random string} |
+| -notest, -n | Skip testing with c2lint (Flag) |
+
+## Custom Wordlists
+If no wordlist is provided, a built-in list will be used by default. For more information about creating these lists, see [below](#building-wordlists) or the [sample wordlists](LINK-HERE) folder in this repo.
+
+| Parameter | Description |
+| ----- | ----- |
+| -charset | File with a custom characterset to use with the %%customchar%% variable |
+| -wordlist | File with a list of custom words to use with the %%word%% variable |
+| -useragent | File with a list of useragents |
+| -spawnto | File with a list of custom spawnto processes |
+| -pipename | File with a list of custome pipenames |
+| -pipename_stager | File with a list of custom pipename_stagers |
+| -dns_stager_subhost | File with a list of custom dns_stager_subhosts |
+
+Most of these wordlist variables are directly related to Malleable C2 options. For more information about what these profile options do, check out the [official documentation](https://www.cobaltstrike.com/help-malleable-c2).
+
+# Substitution Metalanguage
+The substitution metalanguage comprises specific variables, some of which allow optional repitition counts, wrapped in double percentage signs, like so:
+
+```
+%%variable:count%%
+```
+
+As another example, the following variable will result in 20 alphanumeric characters:
+
+```
+%%alphanumeric:20%%
+```
+
+## List of Variables
+
+| Variable | Description | Supports Count? |
+| ----- | ----- | ----- |
+| alphanumeric | Outputs a random mixed-case ascii letter or digit | Yes |
+| alphanumspecial | Outputs a random mixed-case ascii letter, digit, or punctuation | Yes |
+| alphanumspecialurl | Outputs a random mixed-case ascii letter, digit, or one of the following characters: `-._~` | Yes |
+| alphaupper | Outputs a random uppercase ascii letter | Yes |
+| alphalower | Outputs a random lowercase ascii letter | Yes |
+| alpha | Outputs a random ascii letter | Yes |
+| number | Outputs a random digit | Yes |
+| hex | Outputs a random hexadecimal digit | Yes |
+| netbios | Outputs a random mixed-case ascii letter, digit, or one of the following characters: `!@#$%^&)(.-'_{}~` | Yes |
+| custom | Maps to a random character in the provided *charset* file | Yes |
+| word | Outputs a random word from the provided or built-in wordlist | Yes |
+| useragent | Outputs a random useragent from the provided or built-in list | No |
+| spawnto_x86 | Outputs a random x86 process path from the provided or built-in list | No |
+| spawnto_x64 | Outputs a random x64 process path from the provided or built-in list | No |
+| pipename | Outputs a random pipename from the provided or built-in list | No |
+| pipename_stager | Outputs a random pipename_stager from the provided or built-in list | No |
+| dns_stager_subhost | Outputs a random dns_stager_subhost from the provided or built-in list | No |
+
+## Building Wordlists
+Wordlist files are simply line-separated, tab-separated, or continuous strings (depending on the wordlist type) place in a text file.
+
+The following wordlists should be line-separated with each entry on a new line:
+
+* wordlist
+* useragent
+* pipename
+* pipename_stager
+* dns_stager_subhost
+
+The spawnto wordlist is a bit more complicated. Malleable C2 requires an x86 and x64 option to modify all process spawning. Therefore, each line of the wordlist should contain both the x86 and x64 process paths separated by a tab, with the x86 process listed first. For example:
+
+```
+%windir%\\syswow64\\eventvwr.exe	%windir%\\sysnative\\eventvwr.exe
+```
+
+It's important to note that the `syswow64` and `sysnative` strings in the process paths should be **lowercase**.
+
+The final wordlist type is the custom characterset. This file should include any characters for the script to randomly substitute. For example, a charset file of `AEIOUY` and a variable of `%%custom:5%%` will output five random characters from the charset string. When building this characterset, bear in mind that some characters are prohibited from appearing in a URI and may interfere with Beacon's communications.
+
+For sample wordlists, see the [Sample Wordlists](LINK-HERE) directory in this repo.
+
+# Example Template and Output
+EXAMPLE GOES HERE
+
+# Further Resources
+malleable stuff goes here
---
+++ malleable-c2-randomizer.py
@@ -0,0 +1,275 @@
+#!/bin/python
+#author: bluescreenofjeff
+
+#For more information:
+#GITHUB
+#BLOGPOST
+
+import os,argparse,ntpath,random,string,re,subprocess #native
+
+randomize_settings = {}
+processdetails = {}
+
+#Built-in lists
+
+#customcharset
+randomize_settings['customcharset'] = ''
+
+#useragents
+randomize_settings['useragents'] = ['Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)','Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)','Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)','Mozilla/5.0 (Windows NT 10.0; WOW64)','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36','Mozilla/5.0 (Windows NT 6.0; rv:34.0) Gecko/20100101 Firefox/34.0','Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36','Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36','Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1','Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0','Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36','Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:5.0) Gecko/20100101 Firefox/5.0','Mozilla/5.0 (Windows NT 6.1; rv:1.9) Gecko/20100101 Firefox/4.0','Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0','Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36','Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2966.0 Safari/537.36','Mozilla/5.0 (Windows NT 6.3; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0','Mozilla/5.0 (Windows NT 6.3; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0','Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 GTB6 (.NET CLR 3.5.30729)','Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko','Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)','Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1)','Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/6.0)']
+
+#spawnto
+randomize_settings['spawnto'] = {'%windir%\\\\syswow64\\\\eventvwr.exe' : '%windir%\\\\sysnative\\\\eventvwr.exe', '%windir%\\\\syswow64\\\\taskeng.exe' : '%windir%\\\\sysnative\\\\taskeng.exe', '%windir%\\\\syswow64\\\\spoolsv.exe' : '%windir%\\\\sysnative\\\\spoolsv.exe', '%windir%\\\\syswow64\\\\dllhost.exe' : '%windir%\\\\sysnative\\\\dllhost.exe', '%windir%\\\\syswow64\\\\gpupdate.exe' : '%windir%\\\\sysnative\\\\gpupdate.exe'}
+
+#pipename
+randomize_settings['pipename'] = ['lsarpc_##','samr_##','netlogon_##','wkssvc_##','srvsvc_##','mojo_##']
+
+#pipename_stager
+randomize_settings['pipename_stager'] = ['browser_##','comnode_##','spoolss_##','llsrpc_##','comnap_##']
+
+#dns_stager_subhost
+randomize_settings['dns_stager_subhost'] = ['.m.%%number:6%%.','.ftp.%%number:6%%.','.imap.%%number:6%%.','.pop.%%number:6%%.','.smtp.%%number:6%%.','.mail.%%number:6%%.','.webmail.%%number:6%%.','.blog.%%number:6%%.','.wiki.%%number:6%%.','.support.%%number:6%%.','.kb.%%number:6%%.','.help.%%number:6%%.','.go.%%number:6%%.','.static.%%number:6%%.','.api.%%number:6%%.','.dev.%%number:6%%.','.events.%%number:6%%.','.feeds.%%number:6%%.','.forums.%%number:6%%.','.groups.%%number:6%%.','.img.%%number:6%%.','.media.%%number:6%%.','.news.%%number:6%%.','.sites.%%number:6%%.','.admin.%%number:6%%.','.mysql.%%number:6%%.','.store.%%number:6%%.','.vpn.%%number:6%%.','.admin.%%number:6%%.','.beta.%%number:6%%.','.photos.%%number:6%%.','.files.%%number:6%%.','.resources.%%number:6%%.','.secure.%%number:6%%.','.ssl.%%number:6%%.','.apps.%%number:6%%.','.pic.%%number:6%%.','.status.%%number:6%%.','.mobile.%%number:6%%.','.search.%%number:6%%.','.live.%%number:6%%.','.videos.%%number:6%%.','.lists.%%number:6%%.']
+
+#wordlist
+randomize_settings['wordlist'] = ['time','person','year','way','day','thing','man','world','life','hand','part','child','eye','woman','place','work','week','case','point','government','company','number','group','problem','fact']
+
+
+#Setting Defaults
+randomize_settings['count'] = 1
+randomize_settings['cobalt'] = os.getcwd()
+randomize_settings['notest'] = False
+randomize_settings['launchdir'] = os.getcwd()
+
+#process detail counts
+processdetails['errorcount'] = 1
+
+def chargen(charset,number):
+	number = int(number)
+	if charset == 'alphanumeric':
+		return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(number))
+	elif charset == 'alphanumspecial':
+		return ''.join(random.choice(string.ascii_letters + string.digits + string.punctuation) for _ in range(number))
+	elif charset == 'alphanumspecialurl':
+		return ''.join(random.choice(string.ascii_letters + string.digits + '-._~') for _ in range(number))
+	elif charset == 'alpha':
+		return ''.join(random.choice(string.ascii_letters) for _ in range(number))
+	elif charset == 'alphaupper':
+		return ''.join(random.choice(string.ascii_uppercase) for _ in range(number))
+	elif charset == 'alphalower':
+		return ''.join(random.choice(string.ascii_lowercase) for _ in range(number))
+	elif charset == 'number':
+		return ''.join(random.choice(string.digits) for _ in range(number))
+	elif charset == 'hex':
+		return ''.join(random.choice(string.hexdigits) for _ in range(number))
+	elif charset == 'netbios':
+		return ''.join(random.choice(string.ascii_letters + string.digits + "!@#$%^&)(.-'_{}~") for _ in range(number))
+	elif charset == 'customchar':
+		return ''.join(random.choice(randomize_settings['customcharset']) for _ in range(number))
+	elif charset == 'word':
+		return ''.join(random.choice(randomize_settings['wordlist']) for _ in range(number))	
+	else:
+		pass
+
+#PROCESS FUNCTION
+def processprofile(profile):
+	randomize_settings['tempfilename'] = randomize_settings['output'] + '__' + chargen('alphanumeric',8) + '.profile.tmp'			
+
+	filewrite('',randomize_settings['tempfilename'],'w')
+
+	with open(profile) as profilefile:
+		profilecontent = profilefile.read().splitlines()
+
+	spawnto_x86 = random.choice(randomize_settings['spawnto'].keys())
+	spawnto_x64 = randomize_settings['spawnto'][spawnto_x86]
+
+	for line in profilecontent:
+		m = re.findall('%%(alphanumeric|alphanumspecial|alphanumspecialurl|alphaupper|alphalower|alpha|number|hex|netbios|customchar|useragent|spawnto_x86|spawnto_x64|pipename|pipename_stager|dns_stager_subhost|word)\:?(\d*)%%', line)
+		if m:
+			for eachmatch in m:
+				replacetype = eachmatch[0]
+				replacevalue = eachmatch[1]
+
+				#characters
+				if replacetype in ['alphanumeric','alphanumspecial','alphanumspecialurl','alphaupper','alphalower','alpha','number','hex','netbios','customchar','word']:
+					if replacetype == 'customchar' and randomize_settings['customcharset'] == '':
+						print '[!] Error - "customchar" variable called with no customchar file specified:'
+						print '	' + line
+						os._exit(0)
+					if replacevalue == '' or replacevalue == None:
+						line = line.replace('%%' + str(replacetype) + '%%',str(chargen(replacetype,1)),1)
+					else:
+						line = line.replace('%%' + str(replacetype) + ':' + str(replacevalue) + '%%',str(chargen(replacetype,replacevalue)),1)
+
+				elif replacetype == 'useragent':
+					line = line.replace('%%useragent%%',random.choice(randomize_settings['useragents']),1)
+
+				elif replacetype == 'spawnto_x86':
+					line = line.replace('%%spawnto_x86%%',spawnto_x86,1)
+				
+				elif replacetype == 'spawnto_x64':
+					line = line.replace('%%spawnto_x64%%',spawnto_x64,1)
+
+				elif replacetype == 'pipename':
+					line = line.replace('%%pipename%%',random.choice(randomize_settings['pipename']),1)
+
+				elif replacetype == 'pipename_stager':
+					line = line.replace('%%pipename_stager%%',random.choice(randomize_settings['pipename_stager']),1)
+				
+				elif replacetype == 'dns_stager_subhost':
+					line = line.replace('%%dns_stager_subhost%%',random.choice(randomize_settings['dns_stager_subhost']),1)
+				
+
+		filewrite(line + '\n',randomize_settings['tempfilename'],'a')
+
+	#c2lint testing
+	if randomize_settings['notest']:
+		os.system('mv ' + randomize_settings['tempfilename'] + ' ' + randomize_settings['tempfilename'].replace('.tmp','',-1))
+		print '[+] Profile written to: ' + randomize_settings['tempfilename'].replace('.tmp','',-1)
+	elif c2lint():
+		os.system('mv ' + randomize_settings['tempfilename'] + ' ' + randomize_settings['tempfilename'].replace('.tmp','',-1))
+		print '[+] Profile written to: ' + randomize_settings['tempfilename'].replace('.tmp','',-1)
+
+def c2lint():
+	testingoutput = os.popen('cd ' + randomize_settings['cobalt'] + ' && ' + randomize_settings['cobalt'] + 'c2lint ' + randomize_settings['tempfilename']).readlines()
+	fail = False
+	for outputline in testingoutput:
+		if 'Error(s)' in outputline or '[-]' in outputline:
+			fail = True
+		if fail and processdetails['errorcount'] < 4:
+			print '	[-] Error with c2lint check - Attempt ' + str(processdetails['errorcount']) + ' of 3'
+			processdetails['errorcount'] += 1
+			c2lint()
+		elif fail and processdetails['errorcount'] > 3:
+			for _ in testingoutput:
+				print _
+			print '[!] The profile failed c2lint error checking three times. Review the c2lint error report above and modify the profile. Exiting.'
+			os.system('rm ' + randomize_settings['tempfilename'])
+			os._exit(0)
+	processdetails['errorcount'] = 1
+	return True
+
+def filewrite(texttowrite,filetowriteto,mode):
+	f = open(filetowriteto, mode)
+	f.write(texttowrite)
+	f.close()
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description='''malleable-c2-randomizer.py''')
+	parser.add_argument('-profile','-p', help='Path to the Malleable C2 template to randomize', required=True)
+	parser.add_argument('-count','-c', help='The number of randomized profiles to create {Default = 1}')
+	parser.add_argument('-cobalt','-d',  help='The directory where Cobalt Strike is located (for c2lint) {Default = current directory}')
+	parser.add_argument('-output','-o', help='Output base name {Default = template basename and random string}')
+	parser.add_argument('-notest','-n', help='Skip testing with c2lint', action='store_true')
+
+	parser.add_argument('-charset', help='File with a custom characterset to use with the %%customchar%% variable {Default = Built-in list}')
+	parser.add_argument('-wordlist', help='File with a list of custom words to use with the %%word%% variable {Default = Built-in list}')
+	parser.add_argument('-useragent', help='File with a list of useragents {Default = Built-in list}')
+	parser.add_argument('-spawnto', help='File with a list of custom spawnto processes {Default = Built-in list}')
+	parser.add_argument('-pipename', help='File with a list of custome pipenames {Default = Built-in list}')
+	parser.add_argument('-pipename_stager', help='File with a list of custom pipename_stagers {Default = Built-in list}')
+	parser.add_argument('-dns_stager_subhost', help='File with a list of custom dns_stager_subhosts {Default = Built-in list}')
+	
+	args = parser.parse_args()
+	
+	if os.path.isfile(args.profile):
+		randomize_settings['profile'] = args.profile
+	else:
+		parser.error('please specify a valid Malleable C2 profile')
+
+	if args.count != None:
+		try:
+			if int(args.count) > 0:
+				randomize_settings['count'] = int(args.count)
+			else:
+				parser.error('count must be a positive number')
+		except:
+			parser.error('count must be a positive number')
+
+	if args.cobalt:
+		if os.path.isdir(args.cobalt) and os.path.isfile(args.cobalt + '/c2lint'):
+			randomize_settings['cobalt'] = args.cobalt
+		else:
+			parser.error('c2lint not present in current working directory or specified Cobalt Strike directory. Provide a valid Cobalt Strike directory or use the "-notest" flag')
+	elif not args.cobalt and not os.path.isfile(randomize_settings['cobalt'] + '/c2lint'):
+		if not args.notest:
+			parser.error('c2lint not present in current working directory or specified Cobalt Strike directory. Provide a valid Cobalt Strike directory or use the "-notest" flag')
+
+	if args.output:
+		if os.path.isabs(args.output):
+			randomize_settings['output'] = args.output
+		else:
+			randomize_settings['output'] = randomize_settings['launchdir'] + '/' + args.output
+
+	else:
+		if ntpath.basename(args.profile).endswith('.profile'):
+			randomize_settings['output'] = randomize_settings['launchdir'] + '/' + ntpath.basename(args.profile).replace('.profile','',-1)
+		else:
+			randomize_settings['output'] = randomize_settings['launchdir'] + '/' + ntpath.basename(args.profile)
+
+	if args.notest:
+		randomize_settings['notest'] = True
+
+	if args.charset:
+		if os.path.isfile(args.charset):
+			with open(args.charset) as charsetfile:
+				randomize_settings['customcharset'] = ''.join(set(charsetfile.read().replace('\n','').replace('\r','')))
+
+		else:
+			parser.error('the custom charset file specified does not exist')
+
+	if args.wordlist:
+		if os.path.isfile(args.wordlist):
+			with open(args.wordlist) as wordlistfile:
+				randomize_settings['wordlist'] = list(set(wordlistfile.read().splitlines()))
+		else:
+			parser.error('the custom wordlist file specified does not exist')
+
+	if args.useragent:
+		if os.path.isfile(args.useragent):
+			with open(args.useragent) as useragentfile:
+				randomize_settings['useragents'] = list(set(useragentfile.read().splitlines()))
+		else:
+			parser.error('the custom useragent list file specified does not exist')
+
+	if args.spawnto:
+		if os.path.isfile(args.spawnto):
+			randomize_settings['spawnto'] = {}
+			with open(args.spawnto) as spawntofile:
+				lines = spawntofile.read().splitlines()
+				for each in lines:
+					if not each.startswith('#'):
+						randomize_settings['spawnto'][each.split("\t")[0]] = each.split("\t")[1]
+		else:
+			parser.error('the custom spawnto list file specified does not exist')
+
+	if args.pipename:
+		if os.path.isfile(args.pipename):
+			with open(args.pipename) as pipenamefile:
+				randomize_settings['pipename'] = list(set(pipenamefile.read().splitlines()))
+		else:
+			parser.error('the custom pipename list file specified does not exist')
+
+	if args.pipename_stager:
+		if os.path.isfile(args.pipename_stager):
+			with open(args.pipename_stager) as pipenamestagerfile:
+				randomize_settings['pipename_stager'] = list(set(pipenamestagerfile.read().splitlines()))
+		else:
+			parser.error('the custom pipename_stager list file specified does not exist')
+
+	if args.dns_stager_subhost:
+		if os.path.isfile(args.dns_stager_subhost):
+			with open(args.dns_stager_subhost) as dnsstagerfile:
+				randomize_settings['dns_stager_subhost'] = list(set(dnsstagerfile.read().splitlines()))
+		else:
+			parser.error('the custom dns_stager_subhost list file specified does not exist')
+
+	
+	# print args
+	#print randomize_settings
+
+	for iter in range(int(randomize_settings['count'])):
+		iter += 1
+		print '[*] Generating profile ' + str(iter) + ' of ' + str(randomize_settings['count'])
+		processprofile(randomize_settings['profile'])
+	print '[+] Profile randomization complete!'
